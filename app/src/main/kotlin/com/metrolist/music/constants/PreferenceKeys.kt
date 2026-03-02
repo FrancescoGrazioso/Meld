3fb905f3aa9af06f851ea80c7b57dd96ac209389
İsmail TİRYAKİ <ismailtiryaki2021@gmail.com>
2026-03-02 18:47:11 +0300

    feat(playback): add smart sleep timer options and integrate fade-out volume handling

--- app/src/main/kotlin/com/metrolist/music/constants/PreferenceKeys.kt
+++ app/src/main/kotlin/com/metrolist/music/constants/PreferenceKeys.kt
@@ -467,6 +467,8 @@ val LyricsLineSpacingKey = floatPreferencesKey("lyricsLineSpacing")
 
 val PlayerVolumeKey = floatPreferencesKey("playerVolume")
 val SleepTimerDefaultKey = floatPreferencesKey("sleepTimerDefault")
+val SleepTimerStopAfterCurrentSongKey = booleanPreferencesKey("sleepTimerStopAfterCurrentSong")
+val SleepTimerFadeOutKey = booleanPreferencesKey("sleepTimerFadeOut")
 val RepeatModeKey = intPreferencesKey("repeatMode")
 
 val SearchSourceKey = stringPreferencesKey("searchSource")
